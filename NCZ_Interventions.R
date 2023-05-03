0cf46858093b0dc9ad8372f4c718b2933c81d8c2
paulrode <paulrode@outlook.com>
2023-05-02 22:02:24 -0400

    Changing structure of Savings opperations.

--- NCZ_Interventions.R
+++ NCZ_Interventions.R
@@ -231,13 +231,13 @@ for(i in 1:length(Savings$Load)) {
                       if(Savings$Savings[i] == "Heating & Cooling" & Savings$Load[i] == "Elect_kWH" & Savings$`Intervention Name`[i] == "Electrification")  {Savings$`Heating Loads`[i] * Savings$`Change in Electricity Consumption Reduction (kWh)`[i]  -> Savings$Saved_Heating[i]
                           Savings$`Cooling Loads`[i] * Savings$`Change in Electricity Consumption Reduction (kWh)`[i]  -> Savings$Saved_Cooling[i]
                         
-                      }}}}}}}}}}}}}}}}}}}
+                      }}}}}}}}}}}}}}}}}}}}
       
       if (Savings$Load[i] == Savings$Load[i+1] & i< length(Savings$Load))  {Savings$`Base Loads`[i] - Savings$Saved_Base[i] -> Savings$`Base Loads`[i+1]
         if (Savings$Load[i] == Savings$Load[i+1] & i< length(Savings$Load))  {Savings$`Cooling Loads`[i] - Savings$Saved_Cooling[i] -> Savings$`Cooling Loads`[i+1]  
           if (Savings$Load[i] == Savings$Load[i+1] & i< length(Savings$Load))  {Savings$`Heating Loads`[i] - Savings$Saved_Heating[i] -> Savings$`Heating Loads`[i+1]   
             
-          }}}}}
+          }}}}
 
 Savings %>% 
   pivot_wider(names_from = Load, values_from = 4:7) -> Savings2
@@ -245,7 +245,13 @@ Savings %>%
 # I  am going to have to come up with new logic! 5/1  
 #  Logic for electrification. Starting at line 181 for adding logic to electrification measures. Look at making the if statement call up a
 # function that does the calculations. The function would do the following:
-#lets first try a simple funciton that assigns i to and 2i to variables.
+#lets first try a simple function that assigns i to and 2i to variables.
+
+# how about separate out the electrification measures then perform the reductions in savings and then 
+# bringing back the electrification and try to zero it out. 
+
+
+
 
 #  
 # Combinations: 
